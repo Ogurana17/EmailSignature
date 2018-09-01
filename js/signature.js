4b56a5fff9a51f49e26133fe220f08030c7dbb8d
ogurana17 <ogurana17@gmail.com>
2018-09-01 23:43:03 +0900

    Initial commit

--- js/signature.js
+++ js/signature.js
@@ -29,6 +29,7 @@ function create() {
     }
 }
 
+
 function englishCode() {
     signatureEng = '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><br>\n' +
         '<meta http-equiv="Content-Type" content="text/html; charset = UTF-8"><br>\n' +
@@ -36,53 +37,83 @@ function englishCode() {
         '<tr><td>\n' +
         '<table id="info1" style="font-family:sans-serif;font-size:12px;color:#333;border-spacing:0px">';
     
-    signatureEng += '<tr id=email_name_preview style="font-size:15px;font-weight:bold">\n' +
-        '<td colspan="2" style="padding-bottom:5px">' + 'Name' + '</td>\n' +
+    if (name.value) {
+        signatureEng += '<tr id=email_name_preview style="font-size:15px;font-weight:bold">\n' +
+        '<td colspan="2" style="padding-bottom:5px">' + name.value + '</td>\n' +
+        '</tr>\n';    
+    }
+    if (position.value) {
+        signatureEng += '<tr id="position">\n' +
+        '<td colspan="2">' + position.value + '</td>\n' +
         '</tr>\n';
-                    
-    signatureEng += '<tr id="position">\n' +
-        '<td colspan="2">' + 'Position' + '</td>\n' +
-        '</tr>\n';
+    }
 
-    signatureEng += '<tr id="job_company">\n' +
-        '<td colspan="2">' + 'Company' + '</td>\n' +
+    if (company.value) {
+        signatureEng += '<tr id="job_company">\n' +
+        '<td colspan="2">' + company.value + '</td>\n' +
         '</tr>\n';
-
-    signatureEng += '<tr id="phone">\n' +
+    }
+    
+    if (phone.value) {
+        var phoneNum = phone.value;
+        var phoneNumCha = convertToAnchorTag(phoneNum);
+        signatureEng += '<tr id="phone">\n' +
         '<td>Phone: </td>\n' +
-        '<td><a href="tel:' + '0000000000' + '" style="text-decoration:none;color:#001ba0">' + '000-000-0000' + '</a></td>\n' +
+        '<td><a href="tel:' + phoneNumCha + '" style="text-decoration:none;color:#001ba0">' + phone.value + '</a></td>\n' +
         '</tr>\n';
+    }
 
-    signatureEng += '<tr id="mobile">\n' +
+    if (mobile.value) {
+        var mobileNum = mobile.value;
+        var mobileNumCha = convertToAnchorTag(mobileNum);
+        signatureEng += '<tr id="mobile">\n' +
         '<td>Mobile: </td>\n' +
-        '<td><a href="tel:0000000000" style="text-decoration:none;color:#001ba0">000-0000-0000</a></td>\n' +
+        '<td><a href="tel:' + mobileNumCha + '" style="text-decoration:none;color:#001ba0">' + mobile.value + '</a></td>\n' +
         '</tr>\n';
+    }
 
-    signatureEng += '<tr id="fax">\n' +
+    if (fax.value) {
+        signatureEng += '<tr id="fax">\n' +
         '<td>Fax: </td>\n' +
-        '<td>' + '000-000-0000' + '</td>\n' +
+        '<td>' + fax.value + '</td>\n' +
         '</tr>\n';
+    }
 
     signatureEng += '</table>\n' +
         '</td></tr>\n' +
         '<tr><td>\n' +
         '<table id="info2" style="font-family:sans-serif;font-size:12px;color:#333;border-spacing:0px">\n';
 
-    signatureEng += '<tr id="email_website" style="padding:5px 0 0 0 ">\n' +
-        '<td id="email"><a href="mailto:' + 'mail@mail.com' + '" style="text-decoration:none;color:#001ba0">' + 'mail@mail.com' + '</a></td>\n' +
-        '<td id="website">| <a href="' + 'http://www.google.com' + '" style="text-decoration:none;color:#001ba0">' + 'www.google.com' + '</a></td>\n' +
-        '</tr>\n';
+    if (email.value) {
+        signatureEng += '<tr id="email_website" style="padding:5px 0 0 0 ">\n' +
+            '<td id="email"><a href="mailto:' + email.value + '" style="text-decoration:none;color:#001ba0">' + email.value + '</a></td>\n';
+    }
 
-    signatureEng += '<tr style="font-size:10px;padding:5px 0 0 0 ">\n' +
-        '<td colspan="2">' + '1-234-56 cho, shi, ken';
-    
-    signatureEng +=' ' + '123 - 4567' + '</td >\n' +
+    if (web.value) {
+        var webStr = web.value;
+        var webStrChk = webStr.substr(0, 4);
+        if (!(webStrChk === 'http')) {
+            webStr = 'http://' + web.value;
+        }
+        signatureEng += '<td id="website">| <a href="' + webStr + '" style="text-decoration:none;color:#001ba0">' + web.value + '</a></td>\n' +
         '</tr>\n';
+    }
+
+    if (address.value) {
+        signatureEng += '<tr style="font-size:10px;padding:5px 0 0 0 ">\n' +
+        '<td colspan="2">' + address.value;
+    }
+    
+    if (zipCode.value) {
+        signatureEng +=' ' + zipCode.value + '</td >\n' +
+        '</tr>\n';
+    }
 
     signatureEng += '</table>\n' +
         '</td></tr>\n' +
         '</table>';
 }
+
 
 function japaneseCode() {
     signatureJpn = '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><br>\n' +
@@ -105,27 +136,27 @@ function japaneseCode() {
     }
 
     if (zipCode.value) {
-        signatureJpn += '<tr style="width:max-content">\n'
+        signatureJpn += '<tr style="width:max-content">\n' +
         '<td style=text-align:left>〒' + zipCode.value + '</td >\n';
     }
 
     if (address.value) {
-        signatureJpn += '<td><a href="https://www.google.co.jp/maps/place/' + address.value + '" style="text-decoration:none;color:#001ba0">' + address.value + '</a></td>\n'
+        signatureJpn += '<td><a href="https://www.google.co.jp/maps/place/' + address.value + '" style="text-decoration:none;color:#001ba0">' + address.value + '</a></td>\n' +
         '</tr>\n';
     }
 
     if (phone.value) {
         var phoneNum = phone.value;
-        var phoneNumCha = phoneNum.match(/^[0-9]+$/);
+        var phoneNumCha = convertToAnchorTag(phoneNum);
         signatureJpn += '<tr style="width:max-content">\n' +
             '<td style=text-align:left>Tel</td>\n' +
             '<td><a href="tel:' + phoneNumCha + '" style="text-decoration:none;color:#001ba0">' + phone.value + '</a></td>\n' +
             '</tr>\n';
     }
 
-    if (document.getElementById('inputMobile').value) {
+    if (mobile.value) {
         var mobileNum = mobile.value;
-        var mobileNumCha = mobileNum.match(/^[0-9]+$/);
+        var mobileNumCha = convertToAnchorTag(mobileNum);
         signatureJpn += '<tr style="width:max-content">\n' +
             '<td style=text-align:left>Mobile</td>\n' +
             '<td><a href="tel:' + mobileNumCha + '" style="text-decoration:none;color:#001ba0">' + mobile.value + '</a></td>\n' +
@@ -141,7 +172,7 @@ function japaneseCode() {
 
     if (email.value) {
         signatureJpn += '<tr style="width:max-content">\n' +
-            '<td style=text-align:left>E-mail</td>\n' +
+            '<td style=text-align:left>Email</td>\n' +
             '<td><a href=mailto:' + email.value + ' style="text-decoration:none;color:#001ba0">' + email.value + '</a></td>\n' +
             '</tr>\n';
     }
@@ -159,4 +190,24 @@ function japaneseCode() {
     }
 
     signatureJpn += '</table>';
+}
+
+// http://www.de2p.co.jp/tech/html-css-js/tel-to-anchor-tag/
+function convertToAnchorTag( str )
+{
+	// 電話番号だと思われる文字列を抽出
+	var phone_array = str.match( /\+?[0-9]+[\-\x20]?[0-9]+[\-\x20]?[0-9]+[\-\x20]?[0-9]+/g );
+	var cursor = 0;
+	for ( var i = 0; phone_array != null && i < phone_array.length; i++ ) {
+
+		// ハイフンとスペースを削除
+		var tmp = phone_array[i];
+		tmp = tmp.replace( /[\-\x20]/g, '' );
+		if ( tmp.length < 10 ) {
+			// 10桁未満は電話番号とみなさない
+			continue;
+		}
+	}
+
+	return tmp;
 }
