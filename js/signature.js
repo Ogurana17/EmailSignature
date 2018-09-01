219b9315a3ee7e935a62ce6937b705c15a5116b9
ogurana17 <ogurana17@gmail.com>
2018-09-01 10:48:24 +0900

    Initial commit

--- js/signature.js
+++ js/signature.js
@@ -19,9 +19,57 @@ function create() {
 
 function englishCode() {
     signatureEng = '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><br>\n' +
-        '<table style="font-family:sans-serif;font-size:12px;color:#333;border-spacing:0px;border-top:1px solid #333;padding:5px">\n';
+        '<meta http-equiv="Content-Type" content="text/html; charset = UTF-8"><br>\n' +
+        '<table style="font-family:sans-serif;font-size:12px;color:#333;border-spacing:0px;border-top:1px solid #333;padding:5px">\n' +
+        '<tr><td>\n' +
+        '<table id="info1" style="font-family:sans-serif;font-size:12px;color:#333;border-spacing:0px">';
+    
+    signatureEng = '<tr id=email_name_preview style="font-size:15px;font-weight:bold">\n' +
+        '<td colspan="2" style="padding-bottom:5px">' + 'Name' + '</td>\n' +
+        '</tr>\n';
+                    
+    signatureEng = '<tr id="position">\n' +
+        '<td colspan="2">' + 'Position' + '</td>\n' +
+        '</tr>\n';
 
-    signatureEng += '</table>';
+    signatureEng = '<tr id="job_company">\n' +
+        '<td colspan="2">' + 'Company' + '</td>\n' +
+        '</tr>\n';
+
+    signatureEng = '<tr id="phone">\n' +
+        '<td>Phone: </td>\n' +
+        '<td><a href="tel:' + '0000000000' + '" style="text-decoration:none;color:#001ba0">' + '000-000-0000' + '</a></td>\n' +
+        '</tr>\n';
+
+    signatureEng = '<tr id="mobile">\n' +
+        '<td>Mobile: </td>\n' +
+        '<td><a href="tel:0000000000" style="text-decoration:none;color:#001ba0">000-0000-0000</a></td>\n' +
+        '</tr>\n';
+
+    signatureEng = '<tr id="fax">\n' +
+        '<td>Fax: </td>\n' +
+        '<td>' + '000-000-0000' + '</td>\n' +
+        '</tr>\n';
+
+    signatureEng = '</table>\n' +
+        '</td></tr>\n' +
+        '<tr><td>\n' +
+        '<table id="info2" style="font-family:sans-serif;font-size:12px;color:#333;border-spacing:0px">\n';
+
+    signatureEng = '<tr id="email_website" style="padding:5px 0 0 0 ">\n' +
+        '<td id="email"><a href="mailto:' + 'mail@mail.com' + '" style="text-decoration:none;color:#001ba0">' + 'mail@mail.com' + '</a></td>\n' +
+        '<td id="website">| <a href="' + 'http://www.google.com' + '" style="text-decoration:none;color:#001ba0">' + 'www.google.com' + '</a></td>\n' +
+        '</tr>\n';
+
+    signatureEng = '<tr style="font-size:10px;padding:5px 0 0 0 ">\n' +
+        '<td colspan="2">' + '1-234-56 cho, shi, ken';
+    
+    signatureEng =' ' + '343 - 0844' + '</td >\n' +
+        '</tr>\n';
+
+    signatureEng += '</table>\n' +
+        '</td></tr>\n' +
+        '</table>';
 }
 
 function japaneseCode() {
