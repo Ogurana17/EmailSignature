6dee83ae38405710161f291e1478097f466c9487
小倉直己 <oguranaoki@Naoki-MacBook-Pro.local>
2018-12-12 09:05:12 +0900

    add legacy

--- js/signature20181211.js
+++ js/signature20181211.js
@@ -10,7 +10,6 @@ const email = document.getElementById('inputEmail');
 const web = document.getElementById('inputWeb');
 const address = document.getElementById('inputAddress');
 const zipCode = document.getElementById('inputZipCode');
-const country = document.getElementById('inputCountry');
 
 
 function create() {
@@ -128,14 +127,7 @@ function englishCode() {
         if (address.value == "") {
             signatureEng += '<tr style="font-size:10px;padding:5px 0 0 0 ">\n<td>\n';
         }
-        signatureEng += ' ' + zipCode.value;
-    }
-
-    if (country.value) {
-        if (address.value == "") {
-            signatureEng += '<tr style="font-size:10px;padding:5px 0 0 0 ">\n<td>\n';
-        }
-        signatureEng += ' ' + country.value + '</td>\n' +
+        signatureEng +=' ' + zipCode.value + '</td >\n' +
         '</tr>\n';
     }
 
@@ -146,100 +138,80 @@ function englishCode() {
 function japaneseCode() {
     signatureJpn = '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">--\n' +
         '<table style="font-family:sans-serif;font-size:12px;color:#333;border-spacing:5px 0;border-top:1px solid #333;border-bottom:1px solid #333;border-collapse:separate;padding:5px 0;margin:6px 0">\n';
-    
+
+    if (company.value) {
+        signatureJpn += '<tr id=campany style="width:max-content">\n' +
+            '<td colspan="2">' + company.value;
+    }
+
+    if (position.value) {
+        signatureJpn += ' ' + position.value + '</td>\n' +
+            '</tr>\n';
+    }
+
     if (name.value) {
         signatureJpn += '<tr id=email_name_preview style="font-size:15px;font-weight:bold">\n' +
-        '<td>' + name.value + '</td>\n' +
-        '</tr>\n';    
+            '<td colspan="2" style="padding-bottom:2px">' + name.value + '</td>\n' +
+            '</tr>\n';
     }
-    if (position.value) {
-        signatureJpn += '<tr id="position">\n' +
-        '<td>' + position.value + '</td>\n' +
+
+    if (zipCode.value) {
+        signatureJpn += '<tr style="width:max-content">\n' +
+        '<td style=text-align:left>〒' + zipCode.value + '</td >\n';
+    }
+
+    if (address.value) {
+        //signatureJpn += '<td><a href="https://www.google.co.jp/maps/place/' + address.value + '" style="text-decoration:none;color:#1a0dab">' + address.value + '</a></td>\n' +
+        signatureJpn += '<td>' + address.value + '</td>\n' +
         '</tr>\n';
     }
 
-    if (company.value) {
-        signatureJpn += '<tr id="job_company">\n' +
-        '<td>' + company.value + '</td>\n' +
-        '</tr>\n';
-    }
-    
     if (phone.value) {
         var phoneNum = phone.value;
         var phoneNumCha = convertToAnchorTag(phoneNum);
-        signatureJpn += '<tr id="phone">\n' +
-        '<td>Phone: \n' +
-        phone.value + '\n';
-        
-        if (mobile.value == "") {
-            signatureJpn += '</td>\n</tr>\n';
-        }
+        signatureJpn += '<tr style="width:max-content">\n' +
+            '<td style=text-align:left>Tel</td>\n' +
+            //'<td><a href="tel:' + phoneNumCha + '" style="text-decoration:none;color:#1a0dab">' + phone.value + '</a></td>\n' +
+            '<td>' + phone.value + '</td>\n' +
+            '</tr>\n';
     }
 
     if (mobile.value) {
-        if (phone.value == "") {
-            signatureJpn += '<tr id="mobile">\n<td>';
-        }
         var mobileNum = mobile.value;
         var mobileNumCha = convertToAnchorTag(mobileNum);
-        if (phone.value) {
-            signatureJpn += ' | Mobile: \n' + mobile.value + '</td>\n';
-        }
-        else {
-            signatureJpn += 'Mobile: \n' + mobile.value + '</td>\n';
-        }
-        signatureJpn += '</tr>\n';
+        signatureJpn += '<tr style="width:max-content">\n' +
+            '<td style=text-align:left>Mobile</td>\n' +
+            //'<td><a href="tel:' + mobileNumCha + '" style="text-decoration:none;color:#1a0dab">' + mobile.value + '</a></td>\n' +
+            '<td>' + mobile.value + '</td>\n' +
+            '</tr>\n';
     }
 
     if (fax.value) {
-        signatureJpn += '<tr id="fax">\n' +
-        '<td>Fax: \n' +
-        fax.value + '</td>\n' +
-        '</tr>\n';
+        signatureJpn += '<tr style="width:max-content">\n' +
+            '<td style=text-align:left>Fax</td>\n' +
+            '<td>' + fax.value + '</td>\n' +
+            '</tr>\n';
     }
 
     if (email.value) {
-        signatureJpn += '<tr id="email_website" style="padding:5px 0 0 0 ">\n<td>\n' +
-            //'<td id="email"><a href="mailto:' + email.value + '" style="text-decoration:none;color:#1a0dab">' + email.value + '</a></td>\n';
-            email.value + '\n';
-        if (web.value == "") {
-            signatureJpn += '</tr>\n';
-        }
+        signatureJpn += '<tr style="width:max-content">\n' +
+            '<td style=text-align:left>Email</td>\n' +
+            //'<td><a href=mailto:' + email.value + ' style="text-decoration:none;color:#1a0dab">' + email.value + '</a></td>\n' +
+            '<td>' + email.value + '</td>\n' +
+            '</tr>\n';
     }
 
     if (web.value) {
-        if (email.value == "") {
-            signatureJpn += '<tr id="email_website" style="padding:5px 0 0 0 ">\n<td>\n';
-        }
         //var webStr = web.value;
         //var webStrChk = webStr.substr(0, 4);
         //if (!(webStrChk === 'http')) {
         //    webStr = 'http://' + web.value;
         //}
-        //signatureJpn += '<td id="website">| <a href="' + webStr + '" style="text-decoration:none;color:#1a0dab">' + web.value + '</a></td>\n' +
-        if (email.value) {
-            signatureJpn += ' | ' + web.value + '</td>\n';
-        }
-        else {
-            signatureJpn += web.value + '</td>\n';
-        }
-        signatureJpn += '</tr>\n';
-    }
-
-    if (address.value) {
-        signatureJpn += '<tr style="font-size:10px;padding:5px 0 0 0 ">\n' +
-            '<td>' + '〒' + zipCode.value;
-        if (zipCode.value == "") {
-            signatureJpn += '</tr>\n';
-        }
-    }
-
-    if (zipCode.value) {
-        if (address.value == "") {
-            signatureJpn += '<tr style="font-size:10px;padding:5px 0 0 0 ">\n<td>\n';
-        }
-        signatureJpn += ' ' + address.value + '</td>\n' +
-        '</tr>\n';
+        signatureJpn += '<tr style="width:max-content">\n' +
+            '<td style=text-align:left>URL</td>\n' +
+            //'<td><a href=' + webStr + ' style="text-decoration:none;color:#1a0dab">' + web.value + '</a></td>\n' +
+            '<td>' + web.value + '</td>\n' +
+            '</tr>\n';
     }
 
     signatureJpn += '</table>\n<br>';
